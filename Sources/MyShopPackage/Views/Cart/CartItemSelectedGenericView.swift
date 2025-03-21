829aad45211af3ba808e60dcb7d1517c41699031
duongcuong4395 <dangduongcuong@gmail.com>
2025-03-21 14:50:43 +0700

    add CartItemSelectedGenericView

---
+++ Sources/MyShopPackage/Views/Cart/CartItemSelectedGenericView.swift
@@ -0,0 +1,76 @@
+//
+//  CartItemSelectedGenericView.swift
+//  MyShopPackage
+//
+//  Created by Macbook on 21/3/25.
+//
+
+import SwiftUI
+
+public struct CartItemSelectedGenericView<CategoryDT: CategoryData>: View {
+    var uniqueCategories: [CategoryDT]
+    var categoryItemCount: [CategoryDT: Int]
+    var totalPrice: Double
+    
+    var onCancel: () -> Void
+    var onOrder: () -> Void
+    
+    public init(uniqueCategories: [CategoryDT]
+         , categoryItemCount: [CategoryDT : Int]
+         , totalPrice: Double
+         , onCancel: @escaping () -> Void
+         , onOrder: @escaping () -> Void) {
+        self.uniqueCategories = uniqueCategories
+        self.categoryItemCount = categoryItemCount
+        self.totalPrice = totalPrice
+        self.onCancel = onCancel
+        self.onOrder = onOrder
+    }
+    
+    public var body: some View {
+        HStack {
+            ForEach(Array(uniqueCategories.enumerated()), id: \.element.id) { index, cate in
+                Image(cate.imageName)
+                    .resizable()
+                    .padding(5)
+                    .clipShape(Circle())
+                    .frame(width: 35, height: 35)
+                    .overlay {
+                        VStack {
+                            HStack {
+                                Spacer()
+                                Text("\(categoryItemCount[cate] ?? 0)")
+                                    .font(.caption.bold())
+                                    .foregroundStyle(.white)
+                                    .padding(2)
+                                    .clipShape(Circle())
+                                    .background(.red)
+                                
+                            }
+                            Spacer()
+                        }
+                        
+                    }
+            }
+            
+            Spacer()
+            HStack {
+                Text("\(totalPrice.convertMoney())")
+                    .font(.body.bold())
+                Button(action: {
+                    onOrder()
+                }, label: {
+                    Image(systemName: "dollarsign.circle")
+                        .font(.title2.bold())
+                        .foregroundStyle(.yellow)
+                })
+            }
+            .foregroundStyle(.white)
+            .padding(.trailing, 10)
+        }
+        .padding(7)
+        .background(.green, in: RoundedRectangle(cornerRadius: 50, style: .continuous))
+        .padding(.horizontal)
+        .padding(.vertical, 5)
+    }
+}
